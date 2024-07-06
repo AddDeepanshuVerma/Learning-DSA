86a833283792adcfbe14a3d6b72a2bedda62920c
DeepanshuVermag <messagedeepanshuverma@gmail.com>
2024-07-07 04:10:19 +0530

    Recursion series Completed in package : src/Recursion_10

--- src/Recursion_10/GenerateParenthesis_22.java
+++ src/Recursion_10/GenerateParenthesis_22.java
@@ -8,18 +8,21 @@ class GenerateParenthesis_22 {
         System.out.println(generateParenthesis(3));
     }
 
-    public static List<String> generateParenthesis(int n) {
-        List<String> list = new ArrayList<>();
-        getPrintedAllComb("", n, n, list);
-        return list;
-    }
-
     private static void getPrintedAllComb(String op, int open, int close, List<String> list) {
-        if (open == 0 && close == 0) { list.add(op); return; }
+        if (open == 0 && close == 0) {
+            list.add(op);
+            return;
+        }
 
         if (open != 0)
             getPrintedAllComb(op + "(", open - 1, close, list);
         if (close > open)
             getPrintedAllComb(op + ")", open, close - 1, list);
+    }
+
+    public static List<String> generateParenthesis(int n) {
+        List<String> list = new ArrayList<>();
+        getPrintedAllComb("", n, n, list);
+        return list;
     }
 }
