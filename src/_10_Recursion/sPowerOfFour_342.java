43fc84b96842fb2ea6b4935c795390bedfe388f1
DeepanshuVermag <messagedeepanshuverma@gmail.com>
2024-08-02 13:26:25 +0530

    Daily problem

--- src/_10_Recursion/sPowerOfFour_342.java
+++ src/_10_Recursion/sPowerOfFour_342.java
@@ -18,4 +18,12 @@ class sPowerOfFour_342 {
     public boolean isPowerOfFour3(int n) {
         return n > 1 ? n % 4 == 0 && isPowerOfFour3(n / 4) : n == 1;
     }
+
+    public boolean isPowerOfFour4(int n) {
+        if(n <= 0)         return false;
+        if(n == 1)          return true;
+        if(n % 4 != 0 )     return false;
+        return isPowerOfFour4(n / 4);
+    }
+
 }
