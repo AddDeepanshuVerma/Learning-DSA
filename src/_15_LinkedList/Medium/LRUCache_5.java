cfbd21fb04edf4c76ad805d9d511cbf52521e200
DeepanshuVermag <messagedeepanshuverma@gmail.com>
2024-09-03 07:17:59 +0530

    some minute changes

--- src/_15_LinkedList/Medium/LRUCache_5.java
+++ src/_15_LinkedList/Medium/LRUCache_5.java
@@ -4,23 +4,24 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class LRUCache_5 {
-    static class Node {
-        int key;
-
-        int value;
-
-        public Node(int key, int value) {
-            this.key = key;
-            this.value = value;
-        }
-
-    }
 
     private LinkedHashMap<Integer, Integer> map;
     private final int CAPACITY;
 
     public LRUCache_5(int capacity) {
         CAPACITY = capacity;
+
+        /*
+        new LinkedHashMap<Integer,String>(16, .75f, true);
+        With that true you specify that you want an "access-ordered" map, not an "insertion-ordered" map.
+        This means that you will get the values in the order of access (least recently accessed first).
+        Both your get and put calls constitute an "access".
+
+        A special constructor is provided to create a linked hash map whose order of iteration is the order in which its entries were last accessed,
+        from least-recently accessed to most-recently (access-order). This kind of map is well-suited to building LRU caches.
+        Invoking the put or get method results in access to the corresponding entry (assuming it exists after the invocation completes).
+        */
+
         map = new LinkedHashMap<Integer, Integer>(capacity, 0.75f, true) {
             protected boolean removeEldestEntry(Map.Entry eldest) {
                 return size() > CAPACITY;
@@ -32,7 +33,7 @@ public class LRUCache_5 {
         return map.getOrDefault(key, -1);
     }
 
-    public void set(int key, int value) {
+    public void put(int key, int value) {
         map.put(key, value);
     }
 }
