c5c7fd46be06a9ebfb4c783080c7716cbaa900ff
DeepanshuVermag <messagedeepanshuverma@gmail.com>
2024-10-09 00:13:12 +0530

    Title: DSA & ALGORITHM PROGRESS
    Leetcode POTD completed

---
+++ src/DailyCodeChallenge/String/MinSwaps_1963.java
@@ -0,0 +1,20 @@
+package DailyCodeChallenge.String;
+
+class MinSwaps_1963 {
+    public int minSwaps(String s) {
+        int open = 0, close = 0;
+        for (char ch : s.toCharArray()) {
+            switch (ch) {
+                case '[' -> open++;
+                case ']' -> {
+                    if (open > 0) {
+                        open--;
+                    } else {
+                        close++;
+                    }
+                }
+            }
+        }
+        return close;
+    }
+}
