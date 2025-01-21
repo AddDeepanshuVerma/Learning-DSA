4eb56a96152739a641126fcb8a7bb65784254dca
DeepanshuVermag <messagedeepanshuverma@gmail.com>
2025-01-21 14:43:58 +0530

    Leetcode POTD completed

--- src/POTD_LEETCODE/HashTable/FirstCompleteIndex_2661.java
+++ src/POTD_LEETCODE/HashTable/FirstCompleteIndex_2661.java
@@ -1,30 +1,28 @@
 package POTD_LEETCODE.HashTable;
 
 import java.util.HashMap;
-import java.util.Map;
 
 class FirstCompleteIndex_2661 {
     public int firstCompleteIndex(int[] arr, int[][] mat) {
-        int numRows = mat.length, numCols = mat[0].length;
-        int[] rowCount = new int[numRows], colCount = new int[numCols];
-        Map<Integer, int[]> map = new HashMap<>();
-
-        for (int row = 0; row < numRows; row++) {
-            for (int col = 0; col < numCols; col++) {
-                int value = mat[row][col];
-                map.put(value, new int[]{row, col});
+        HashMap<Integer, int[]> map = new HashMap<>();
+        int m = mat.length, n = mat[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                map.put(mat[i][j], new int[]{i, j});
             }
         }
 
+        int[] rowCount = new int[m];
+        int[] colCount = new int[n];
+
         for (int i = 0; i < arr.length; i++) {
-            int[] pos = map.get(arr[i]);
-            rowCount[pos[0]]++;
-            colCount[pos[1]]++;
-            if (rowCount[pos[0]] == numCols || colCount[pos[1]] == numRows) {
+            int[] grid = map.get(arr[i]);
+            rowCount[grid[0]]++;
+            colCount[grid[1]]++;
+            if (rowCount[grid[0]] == n || colCount[grid[1]] == m) {
                 return i;
             }
         }
-
         return -1;
     }
 }
