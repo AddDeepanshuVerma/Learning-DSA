f6bdfb33edc25646c5fac1e09e047d648f7489c7
DeepanshuVermag <messagedeepanshuverma@gmail.com>
2025-02-20 03:03:19 +0530

    Title: GRAPH Starts!!!
    Done some basic graph conceptual probs.

--- src/_21_Graphs/Algo_MikePlayList/_3Traversal_BFS.java
+++ src/_21_Graphs/Algo_MikePlayList/_3Traversal_BFS.java
@@ -1,9 +1,6 @@
 package _21_Graphs.Algo_MikePlayList;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class _3Traversal_BFS {
     public static void main(String[] args) {
@@ -14,6 +11,8 @@ public class _3Traversal_BFS {
         System.out.println("dfs = " + dfs);
     }
 
+    // ========================= using adj Lists(map) =========================
+
     private static ArrayList<Integer> dfsOfGraph(List<List<Integer>> adj) {
         // create map ( tho we can use the given structure as well)
         HashMap<Integer, List<Integer>> map = new HashMap<>();
@@ -22,12 +21,53 @@ public class _3Traversal_BFS {
                 map.computeIfAbsent(u, list -> new ArrayList<Integer>()).add(v);
             }
         }
-        ArrayList<Integer> ans = bfs(map);
+        return bfs(adj.size(), map);
+    }
+
+    private static ArrayList<Integer> bfs(int size, HashMap<Integer, List<Integer>> map) {
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+        boolean[] visited = new boolean[size];
+        ArrayList<Integer> ans = new ArrayList<>();
+        q.offer(0);
+        visited[0] = true;
+
+        while (!q.isEmpty()) {
+            int poll = q.poll();
+            ans.add(poll);
+            for (Integer next : map.getOrDefault(poll, Collections.emptyList())) {
+                if (visited[next]) continue;
+                visited[next] = true;
+                q.offer(next);
+            }
+        }
         return ans;
     }
 
-    private static ArrayList<Integer> bfs(HashMap<Integer, List<Integer>> map) {
-        return null;
+    // ========================= using adj matrix =========================
+
+    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        // code here
+        return solve(V, adj);
     }
 
+    private ArrayList<Integer> solve(int v, ArrayList<ArrayList<Integer>> adj) {
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+        boolean[] visited = new boolean[v];
+        q.offer(0);
+        visited[0] = true;
+
+        while (!q.isEmpty()) {
+            int poll = q.poll();
+            ans.add(poll);
+
+            for (Integer next : adj.get(poll)) {
+                if (visited[next]) continue;
+                visited[next] = true;
+                q.offer(next);
+            }
+        }
+        return ans;
+    }
 }
