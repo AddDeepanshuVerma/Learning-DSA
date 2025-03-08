05dfd90ea922426479b1e7c414fedc900eb7ae42
DeepanshuVermag <messagedeepanshuverma@gmail.com>
2025-03-08 12:50:56 +0530

    Title: GRAPH COMPLETED !!!
    Graph concept completed with this
    Multi Source BFS topic.
    
    Did learn a lot, now will be starting with Graph questions playList.

---
+++ src/_21_Graphs/Algo_MikePlayList/_23_MultiSourceBFS.java
@@ -0,0 +1,63 @@
+package _21_Graphs.Algo_MikePlayList;
+
+import java.util.ArrayDeque;
+
+public class _23_MultiSourceBFS {
+    /*
+     * Definition : It is nothing but Breadth First Search but starting with multiple source
+     *
+     * When to use :
+     * It starts simultaneously from multiple sources/nodes.
+     * Useful when you want to spread out from multiple source at once.
+     * When you need to compute the shortest distance from multiple starting points to other points in graph
+     *
+     * NOTE: Multi source BFS ensures that all nodes are reached with minimal time from nearest source.
+     *
+     * How to do :
+     * As in BFS we initially start BFS from one point here we start BFS from multiple points hence initially with a given
+     * condition satisfied, we put them in the que and run the BFS as per queue size again and again with visited structure to
+     * not visit the same visited node.
+     * */
+
+    public static int[][] MultiSourceBFS(int[][] grid) {
+        // Let problem says all the index having 0 value will be sources
+        int n = grid.length;
+        int[][] dirs = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+        boolean[][] visited = new boolean[n][n];
+        ArrayDeque<Pair> q = new ArrayDeque<Pair>();
+
+        // Put all those sources to the queue at the start point
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                if (grid[row][col] == 0) {
+                    q.addLast(new Pair(row, col)); // adding all multiple source initially
+                    visited[row][col] = true;      // marking all sources visited
+                }
+            }
+        }
+
+        // now start the BFS from all those sources as the same time
+        while (!q.isEmpty()) {
+            int size = q.size();
+
+            while (size-- > 0) {
+                Pair poll = q.removeFirst();
+                int uRow = poll.row;
+                int uCol = poll.col;
+
+                for (int[] dir : dirs) {
+                    int vRow = uRow + dir[0];
+                    int vCol = uCol + dir[1];
+                    if (vRow < 0 || vRow >= n || vCol < 0 || vCol >= n || visited[vRow][vCol]) continue;
+                    visited[vRow][vCol] = true;
+                    q.addLast(new Pair(vRow, vCol));
+                    // here we have all those node that are reached first
+                }
+            }
+        }
+        return grid;
+    }
+
+    record Pair(Integer row, Integer col) {
+    }
+}
---
+++ src/_21_Graphs/Leetcode/_23_UpdateMatrix_542.java
@@ -0,0 +1,40 @@
+package _21_Graphs.Leetcode;
+
+import java.util.ArrayDeque;
+
+class _23_UpdateMatrix_542 {
+    // Multi Source BFS
+    public int[][] updateMatrix(int[][] mat) {
+        int m = mat.length, n = mat[0].length;
+        int[][] dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        ArrayDeque<int[]> dQue = new ArrayDeque<>();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 0) {
+                    dQue.addLast(new int[]{0, i, j});
+                } else {
+                    mat[i][j] = -1;
+                }
+            }
+        }
+
+        while (!dQue.isEmpty()) {
+            int size = dQue.size();
+            while (size-- > 0) {
+                int[] poll = dQue.removeFirst();
+                int val = poll[0], row = poll[1], col = poll[2];
+                for (int[] dir : dirs) {
+                    int nRow = row + dir[0];
+                    int nCol = col + dir[1];
+                    if (nRow < 0 || nRow >= m || nCol < 0 || nCol >= n || mat[nRow][nCol] != -1) {
+                        continue;
+                    }
+                    dQue.addLast(new int[]{val + 1, nRow, nCol});
+                    mat[nRow][nCol] = val + 1;
+                }
+            }
+        }
+        return mat;
+    }
+}
