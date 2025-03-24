e2789e08a93d19e8ecf18689ebaafb963f0622d1
DeepanshuVermag <messagedeepanshuverma@gmail.com>
2025-03-24 14:44:49 +0530

    some random changes

---
+++ src/Braces.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.util.Scanner;
+
+public class Braces {
+    public static void main(String[] args) {
+        String str = new Scanner(System.in).nextLine();
+        str = str.replace('[', '{');
+        str = str.replace(']', '}');
+
+        StringSelection selection = new StringSelection(str);
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.setContents(selection, selection);
+    }
+}
